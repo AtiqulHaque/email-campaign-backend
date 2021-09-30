275be8fa129e0dc5fca40291a5e0401a74dbdaf5
atiqul <md_atiqulhaque@yahoo.com>
2021-10-01 01:19:18 +0600

    update nodejs

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const aupairRouter = require("./routers/aupairRouter");
-const hostFamilyRouter = require("./routers/hostFamilyRouter");
-const authRouter = require("./routers/authRouter");
+// const aupairRouter = require("./routers/aupairRouter");
+// const hostFamilyRouter = require("./routers/hostFamilyRouter");
+// const authRouter = require("./routers/authRouter");
 const { home } = require("./controllers/welcomeController");
 
 const {
@@ -20,27 +20,27 @@ app.use(cors());
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("database connection successful!"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_CONNECTION_STRING, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("database connection successful!"))
+//   .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // parse cookies
-app.use(cookieParser(process.env.COOKIE_SECRET));
+//app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get("/", home);
 
-app.use("/user", authRouter);
-
-app.use("/aupair", aupairRouter);
-
-app.use("/host-family", hostFamilyRouter);
+// app.use("/user", authRouter);
+//
+// app.use("/aupair", aupairRouter);
+//
+// app.use("/host-family", hostFamilyRouter);
 
 //Not found Hnadler
 app.use(notFoundHandler);
@@ -48,6 +48,6 @@ app.use(notFoundHandler);
 // common error handler
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
-  console.log(`app listening to port ${process.env.PORT}`);
+app.listen("3000", () => {
+  console.log(`app listening to port 3000`);
 });
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,5 +7,21 @@ mkdir $LOGFOLDER
 touch $LOG
 /bin/echo "$(date '+%Y-%m-%d %X') : ** Before Install Hook Started **" >> $LOG
 
+# Install node.js
+sudo yum install python-software-properties -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
+sudo apt-get update
+sudo apt-get install nodejs -y
+
+# Install nodemon
+# sudo npm install nodemon -g
+
+# Install forever module
+# https://www.npmjs.com/package/forever
+sudo npm install forever -g
+
+# Clean working folder
+# sudo find /home/ubuntu/test -type f -delete
+
 
 /bin/echo "$(date '+%Y-%m-%d %X') : ** Before Install Hook Completed **" >> $LOG
